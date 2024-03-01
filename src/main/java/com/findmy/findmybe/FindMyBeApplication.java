bbe51fe0a1382219b64499f7c276a279e55a990c
NikiKaloyanov <nikolaikaloianov@gmail.com>
2024-03-02 01:00:01 +0200

    Initial commit

---
+++ src/main/java/com/findmy/findmybe/FindMyBeApplication.java
@@ -0,0 +1,13 @@
+package com.findmy.findmybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindMyBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/findmy/findmybe/FindMyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findmy.findmybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
